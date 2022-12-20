52430dc3e8616f0f44041ad68cbd2dc4bbb91bc9
hakan-persson <hakan.persson@haninge.se>
2022-12-20 11:29:13 +0100

    Added Excel attachments

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ from elasticsearch.exceptions import NotFoundError
 
 from jinja2 import Environment, FileSystemLoader
 
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import PatternFill, Font
+from openpyxl.utils.cell import get_column_letter
+from openpyxl.writer.excel import save_virtual_workbook
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 from msgraph.core import GraphClient
 from azure.identity import ClientSecretCredential
@@ -74,15 +80,59 @@ def mk_html(data, header, template):
 
 
 """
+Make Excel with details
+"""
+def mk_excel(data, sheet_title):
+    normal_font=Font(name="Arial", size=11, bold=False)
+#     bold_font=Font(name="Arial", size=11, bold=True)
+    yellow_fill = PatternFill("solid", fgColor="FFFF00")
+
+    wb = Workbook()
+    ws = wb.active
+    ws.title = sheet_title
+
+    columns = [("Datum",15)]
+    plist = []
+    for p in data.values():
+        for process in p.keys():
+            if process not in plist:
+                plist.append(process)
+                columns.append((process,20))
+
+    for col in range(len(columns)):
+        ws.cell(row=1,column=col+1).value = columns[col][0]
+        ws.cell(row=1,column=col+1).font = normal_font
+        ws.cell(row=1,column=col+1).fill = yellow_fill
+        ws.column_dimensions[get_column_letter((col+1))].width = columns[col][1]
+    r = 2
+
+    for day, p in data.items():
+        ws.cell(row=r,column=1).value = day
+        for process, cnt in p.items():
+            ws.cell(row=r,column=2+plist.index(process)).value = cnt
+        r += 1
+
+    xlsx_data = save_virtual_workbook(wb)
+    # with open("lista.xlsx","wb") as f:
+    #     f.write(xlsx_data)
+    # f.close()
+    return xlsx_data
+
+
+"""
 Send report as mail
 """
-def send_mail(subject, html):
+def send_mail(subject, html, xlsx_data=None):
         message = MIMEMultipart('alternative')          # Create a MIME message
         message["From"] = "NoReply@haninge.se"
         message["To"] = REPORT_RECIPIENT
         message["Subject"] = subject
         message.attach(MIMEText("Kontakta digit@haninge.se om du ser den här texten!", 'plain'))
         message.attach(MIMEText(html, 'html'))      # Add the HTML formatted content
+        if xlsx_data:
+            attachment = MIMEApplication(xlsx_data,'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            attachment.add_header('Content-Disposition', 'attachment', filename=f"rapport.xlsx")
+            message.attach(attachment)
 
         try:
             credential = ClientSecretCredential(AD_TENANT_ID, AD_CLIENT_ID, AD_CLIENT_SECRET)
@@ -112,7 +162,7 @@ def main():
     report_template = jinja_env.get_template("report.jinja2.html")
 
     today = date.today()
-    # today = date(2022,12,19)
+    today = date(2022,12,19)
     # today = date(2023,1,1)
     if today.day == 1:      # First day of the month. Run a montly report on the previous mounth
         startday = date(today.year, today.month-1, 1) if today.month != 1 else date(today.year-1, 12, 1)
@@ -120,14 +170,16 @@ def main():
         data = get_data(datelist)
         header = f"Camunda månadsrapport för {startday.strftime('%B %Y')}"
         html = mk_html(data, header, report_template)
-        send_mail(header, html)
+        xlsx = mk_excel(data, startday.strftime('%Y-%m'))
+        send_mail(header, html, xlsx)
 
     if today.weekday() == 0:        # Today is Monday. Run a report on the previous week
         datelist = [(today+timedelta(days=d)).isoformat() for d in range(-7,0)]
         data = get_data(datelist)
         header = f"Camunda veckorapport för vecka {(today+timedelta(days=-1)).strftime('%W %Y')}"
         html = mk_html(data, header, report_template)
-        send_mail(header, html)
+        xlsx = mk_excel(data, (today+timedelta(days=-1)).strftime('%W %Y'))
+        send_mail(header, html, xlsx)
 
     yesterday = (today+timedelta(-1)).isoformat()
     data = get_data([yesterday])      # Always run a report on the previous day
